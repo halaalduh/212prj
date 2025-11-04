0749c3b07ac3c867ce71ce8f0131b033d8eada21
hcchly <haifathefirst@gmail.com>
2025-11-05 01:55:45 +0300

    Update Reviews.java

--- Rand'sGPT/Reviews.java
+++ Rand'sGPT/Reviews.java
@@ -1,3 +1,5 @@
+//class is ready
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -57,3 +59,4 @@ public class Reviews {
                 ", comment='" + comment + '\'' +
                 '}';
     }}
+
